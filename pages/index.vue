0c86af2ce788c1cf9619bbad2ade6b8130f6c51f
Gerrit Plehn <gerritplehn@gmail.com>
2022-02-05 16:52:06 +0100

    add more components

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <blog-c theme="indigo" />
-    <blog-b theme="red" />
-    <blog-a theme="yellow" />
+    <contact-a theme="indigo" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import BlogA from '~/components/BlogA.vue'
-import BlogB from '~/components/BlogB.vue'
-import BlogC from '~/components/BlogC.vue'
+import ContactA from '~/components/contact/ContactA.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  components: { BlogB, BlogA, BlogC },
+  components: { ContactA },
 })
 </script>
