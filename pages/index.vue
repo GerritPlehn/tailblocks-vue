12f4d1e7b4826c9ee2f9e5197db86272214bd25c
Gerrit Plehn <gerritplehn@gmail.com>
2022-02-05 10:03:28 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <blog-c theme="indigo" />
+    <blog-b theme="red" />
+    <blog-a theme="yellow" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BlogA from '~/components/BlogA.vue'
+import BlogB from '~/components/BlogB.vue'
+import BlogC from '~/components/BlogC.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: { BlogB, BlogA, BlogC },
+})
+</script>
