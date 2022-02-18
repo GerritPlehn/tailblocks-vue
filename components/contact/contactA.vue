7192062b934e4234de784f7088e0743d9ffba0da
Gerrit Plehn <gerritplehn@gmail.com>
2022-02-18 23:11:09 +0100

    regenerate components

---
+++ components/contact/contactA.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="text-gray-600 body-font relative dark:text-gray-400 dark:bg-gray-900">
+    <div class="absolute inset-0 bg-gray-300 dark:bg-gray-900">
+      <iframe
+        width="100%"
+        height="100%"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        title="map"
+        scrolling="no"
+        src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+        style="filter: grayscale(1) contrast(1.2) opacity(0.4)"
+      ></iframe>
+    </div>
+    <div class="container px-5 py-24 mx-auto flex">
+      <div
+        class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md dark:bg-gray-900"
+      >
+        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font dark:text-white">Feedback</h2>
+        <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+        <div class="relative mb-4">
+          <label for="email" class="leading-7 text-sm text-gray-600 dark:text-gray-400">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            :class="`w-full bg-white rounded border border-gray-300 focus:border-${theme}-500 focus:ring-2 focus:ring-${theme}-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out dark:bg-gray-800 dark:border-gray-700 dark:focus:ring-${theme}-900 dark:text-gray-100`"
+          />
+        </div>
+        <div class="relative mb-4">
+          <label for="message" class="leading-7 text-sm text-gray-600 dark:text-gray-400">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            :class="`w-full bg-white rounded border border-gray-300 focus:border-${theme}-500 focus:ring-2 focus:ring-${theme}-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out dark:bg-gray-800 dark:border-gray-700 dark:focus:ring-${theme}-900 dark:text-gray-100`"
+          ></textarea>
+        </div>
+        <button
+          :class="`text-white bg-${theme}-500 border-0 py-2 px-6 focus:outline-none hover:bg-${theme}-600 rounded text-lg`"
+        >
+          Button
+        </button>
+        <p class="text-xs text-gray-500 mt-3 dark:text-gray-400 dark:text-opacity-90">
+          Chicharrones blog helvetica normcore iceland tousled brook viral artisan.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'red',
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ components/team/teamA.vue
@@ -0,0 +1,145 @@
+<template>
+  <section class="text-gray-600 body-font dark:text-gray-400 dark:bg-gray-900">
+    <div class="container px-5 py-24 mx-auto">
+      <div class="flex flex-col text-center w-full mb-20">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 dark:text-white">Our Team</h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+          Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.
+          Franzen you probably haven't heard of them.
+        </p>
+      </div>
+      <div class="flex flex-wrap -m-2">
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/80x80"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Holden Caulfield</h2>
+              <p class="text-gray-500 dark:text-gray-600">UI Designer</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/84x84"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Henry Letham</h2>
+              <p class="text-gray-500 dark:text-gray-600">CTO</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/88x88"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Oskar Blinde</h2>
+              <p class="text-gray-500 dark:text-gray-600">Founder</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/90x90"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">John Doe</h2>
+              <p class="text-gray-500 dark:text-gray-600">DevOps</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/94x94"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Martin Eden</h2>
+              <p class="text-gray-500 dark:text-gray-600">Software Engineer</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/98x98"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Boris Kitua</h2>
+              <p class="text-gray-500 dark:text-gray-600">UX Researcher</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/100x90"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Atticus Finch</h2>
+              <p class="text-gray-500 dark:text-gray-600">QA Engineer</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/104x94"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Alper Kamu</h2>
+              <p class="text-gray-500 dark:text-gray-600">System</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg dark:border-gray-800">
+            <img
+              alt="team"
+              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+              src="https://dummyimage.com/108x98"
+            />
+            <div class="flex-grow">
+              <h2 class="text-gray-900 title-font font-medium dark:text-white">Rodrigo Monchi</h2>
+              <p class="text-gray-500 dark:text-gray-600">Product Manager</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: 'red',
+    },
+  },
+}
+</script>
+
+<style></style>
